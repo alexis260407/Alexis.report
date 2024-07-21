f8dd069d5bdbabb4266b2d4e21b9b09e58569b9c
Alexis <85355477+alexis260407@users.noreply.github.com>
2024-07-21 23:36:40 +0200

    ajout d'un PlayerListener

--- src/main/java/fr/alexis/report/Main.java
+++ src/main/java/fr/alexis/report/Main.java
@@ -3,6 +3,7 @@ package fr.alexis.report;
 import fr.alexis.report.commands.*;
 import fr.alexis.report.listeners.ButtonListener;
 import fr.alexis.report.commands.SetButtonCommand;
+import fr.alexis.report.listeners.PlayerListener;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.alexis.report.listeners.MenuListener;
 
@@ -10,10 +11,14 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        //Report Command + Listener
         getServer().getPluginManager().registerEvents(new MenuListener(), this);
         getCommand("report").setExecutor(new Reportcommand());
+        //Bouton Command + Listener
         getServer().getPluginManager().registerEvents(new ButtonListener(this), this);
         getCommand("setbutton").setExecutor(new SetButtonCommand(this));
+        //PlayerListener
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         System.out.println("Plugin Allumé");
     }
 
