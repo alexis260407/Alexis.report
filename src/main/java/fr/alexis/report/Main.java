817e6568f5147efef5c3f956d1e3a0aa1258050e
Alexis <85355477+alexis260407@users.noreply.github.com>
2024-07-27 15:41:27 +0200

    Add PlayerListener and evenement. But ScoreBoard.java have some ping (tps 19.99 :/)

--- src/main/java/fr/alexis/report/Main.java
+++ src/main/java/fr/alexis/report/Main.java
@@ -1,14 +1,9 @@
 package fr.alexis.report;
 
-import fr.alexis.report.Scoreboard.ScoreBoard;
 import fr.alexis.report.commands.*;
 import fr.alexis.report.events.Evenement;
 import fr.alexis.report.listeners.ButtonListener;
 import fr.alexis.report.commands.SetButtonCommand;
-import fr.alexis.report.listeners.PlayerListener;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import fr.alexis.report.listeners.MenuListener;
 
@@ -24,7 +19,7 @@ public final class Main extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new ButtonListener(this), this);
         getCommand("setbutton").setExecutor(new SetButtonCommand(this));
         //PlayerListener
-        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        //getServer().getPluginManager().registerEvents(new PlayerListener(), this);
         getServer().getPluginManager().registerEvents(new Evenement(), this);
 
         System.out.println("Plugin Allumé");
