23ce6b642be811e29848a8bfc9c06250e2137195
Alexis <85355477+alexis260407@users.noreply.github.com>
2024-07-27 17:20:34 +0200

    modification du scoreboard

--- src/main/java/fr/alexis/report/Scoreboard/ScoreBoard.java
+++ src/main/java/fr/alexis/report/Scoreboard/ScoreBoard.java
@@ -1,31 +1,39 @@
 package fr.alexis.report.Scoreboard;
 
+
+import com.avaje.ebean.validation.NotNull;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.DisplaySlot;
-import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
-import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.*;
 
 public class ScoreBoard {
-    public static void SetScoreBoard(Player player){
-        //aucune idée a quoi sert ?
+    @EventHandler
+    public void onPlayerJoinBoard(@NotNull PlayerJoinEvent event) {
+        //permet de créer un scoreboard en event pour le player.
+        createBoard(event.getPlayer());
+    }
+
+    public void createBoard(@NotNull Player player) {
         ScoreboardManager manager = Bukkit.getScoreboardManager();
-
-        org.bukkit.scoreboard.Scoreboard board = manager.getNewScoreboard();
-        //objective = l'objectif ?
-        Objective obj = board.registerNewObjective("test", "dummy");
-        //tout en haut
-        obj.setDisplayName("§cBienvenue !");
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
-
-        Score score1 = obj.getScore("Test");
-        score1.setScore(0);
-
-        Score score2 =obj.getScore("Test d'un grand");
-        score2.setScore(1);
+        assert manager != null;
+        Scoreboard board = manager.getNewScoreboard();
+        //permet de créer les lignes ?
+        Objective o = board.registerNewObjective("scores", "dummy", ChatColor.GREEN + "§1Test");
+        o.setDisplaySlot(DisplaySlot.SIDEBAR);
 
         player.setScoreboard(board);
-
+    }
+    public void addLineToSb(Player p) {
+        Scoreboard board = p.getScoreboard();
+        Team myData = board.registerNewTeam("myData");
+        myData.setPrefix(ChatColor.RED + "Mon incroyable data ?");
+        Objective.getScore(ChatColor.BLUE + "").setScore(14);
+    }
+    public void update(Player p) { //Permet d'actualiser le scoreboard
+        Scoreboard board = p.getScoreboard();
+        board.getTeam("myData").setPrefix(ChatColor.RED + "Mon incroyable data (2) ?");
     }
 }
