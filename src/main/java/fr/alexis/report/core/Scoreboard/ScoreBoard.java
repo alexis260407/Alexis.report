70a208d50bc14024e4a251af206968473ebbcbde
Alexis <85355477+alexis260407@users.noreply.github.com>
2024-08-13 13:33:21 +0200

    Ajout du mode online (spigot)

--- src/main/java/fr/alexis/report/core/Scoreboard/ScoreBoard.java
+++ src/main/java/fr/alexis/report/core/Scoreboard/ScoreBoard.java
@@ -30,31 +30,29 @@ public class ScoreBoard implements Listener {
 
 	public static void addLineToSb(Player p, Scoreboard board, Objective obj) {
 		Team myData = board.registerNewTeam("myData");
+		Team online = board.registerNewTeam("online");
+		online.addEntry("t");
+		if (Bukkit.getOnlinePlayers().size() == 0) {
+			online.setPrefix(ChatColor.DARK_RED + "0" + ChatColor.RED + "/" + Bukkit.getMaxPlayers());
+		} else {
+			online.setPrefix("§fJoueurs " + ChatColor.GREEN + Bukkit.getOnlinePlayers().size());
+		}
 		//obj.getScore + .setScore permet d'ajouter une ligne au scoreboard avec le chiffre attribué
 		obj.getScore("§cServeur.fr").setScore(1);
 		obj.getScore("   ").setScore(2);
 		obj.getScore("En jeu" + ChatColor.RED).setScore(3);
-		obj.getScore("Joueurs" + ChatColor.RED).setScore(4);
+		obj.getScore("t").setScore(4);
 		obj.getScore("AFK").setScore(5);
 		obj.getScore("  ").setScore(6);
 		obj.getScore("➜ Pièce :").setScore(7);
 		obj.getScore("➜ Grade").setScore(8);
 		obj.getScore("➜ Pseudo:").setScore(9);
-		obj.getScore("").setScore(10);
+		obj.getScore("§c").setScore(10);
 	}
 
 	public static void update(Player p) { // Permet d'actualiser le scoreboard (actuellement pas encore utilisé)
 		Scoreboard board = p.getScoreboard();
 		board.getTeam("myData").setPrefix(ChatColor.RED + "Test 2");
-	}
-	public static void OnlinePlayer (Player p, Scoreboard board, Objective obj){
-		Team onlineCounter = board.registerNewTeam("onlineCounter");
-		// Note: The ' + "" + ' is there to convert the colors into strings without manually calling ChatColor#toString
-		onlineCounter.addEntry(ChatColor.BLACK + "" + ChatColor.WHITE);
-		if (Bukkit.getOnlinePlayers().size() == 0) {
-			onlineCounter.setPrefix(ChatColor.RED + "0");
-		} else {
-			onlineCounter.setPrefix("" + ChatColor.DARK_RED + Bukkit.getOnlinePlayers().size() + ChatColor.RED + "/" + ChatColor.DARK_RED + Bukkit.getMaxPlayers());
-		}
+		
 	}
 }
